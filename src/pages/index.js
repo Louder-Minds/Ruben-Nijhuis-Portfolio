9046f0fc917274eb7aa30e105c31db268fbffd93
Ruben Nijhuis <contact@rubennijhuis.com>
2021-02-18 14:17:22 +0100

    🎉:feat implemented components into pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,86 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Layout from 'components/layout';
 import { graphql } from 'gatsby';
-import ContentCardGrid from '../containers/content-card-grid';
+import ContentCardGrid from 'containers/content-card-grid';
 import StoryBlock from 'containers/story-block';
-
-const testData = [
-  {
-    title: 'Studio Kffein',
-    tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
-    img: '',
-    subjects: ['Lorem ipsum', 'Lorem ipsum', 'Lorem ipsum'],
-    type: 'projects',
-    full: true,
-  },
-  {
-    title: 'Studio Kffein',
-    tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
-    img: '',
-    subjects: ['Lorem ipsum', 'Lorem ipsum', 'Lorem ipsum'],
-    type: 'projects',
-    full: false,
-  },
-  {
-    title: 'Studio Kffein',
-    tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
-    img: '',
-    subjects: ['Lorem ipsum', 'Lorem ipsum', 'Lorem ipsum'],
-    type: 'projects',
-    full: false,
-  },
-  {
-    title: 'Studio Kffein',
-    tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
-    img: '',
-    subjects: ['Lorem ipsum', 'Lorem ipsum', 'Lorem ipsum'],
-    type: 'projects',
-    full: false,
-  },
-  {
-    title: 'Studio Kffein',
-    tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
-    img: '',
-    subjects: ['Lorem ipsum', 'Lorem ipsum', 'Lorem ipsum'],
-    type: 'projects',
-    full: false,
-  },
-];
+import { contentCardTestData } from 'constants/theme';
+import Header from '../components/header';
 
 const Index = ({ data }) => (
-  <Layout>
-    <ContentCardGrid projects={testData} />
-    <StoryBlock />
-  </Layout>
+    <Layout>
+        <Header
+            title={`Portfolio <br/> 2020 <span>•</span> 2021`}
+            secondaryTitle={`Ad <br/> Explorandum`}
+            light={true}
+        />
+        <ContentCardGrid projects={contentCardTestData} />
+        <StoryBlock />
+    </Layout>
 );
 
 Index.propTypes = {
-  data: PropTypes.object.isRequired,
+    data: PropTypes.object.isRequired,
 };
 
 export default Index;
 
 export const query = graphql`
-  query HomepageQuery {
-    homeJson {
-      title
-      content {
-        childMarkdownRemark {
-          html
-          rawMarkdownBody
-        }
-      }
-      gallery {
-        title
-        copy
-        image {
-          childImageSharp {
-            fluid(maxHeight: 500, quality: 90) {
-              ...GatsbyImageSharpFluid_withWebp
+    query HomepageQuery {
+        homeJson {
+            title
+            content {
+                childMarkdownRemark {
+                    html
+                    rawMarkdownBody
+                }
             }
-          }
+            gallery {
+                title
+                copy
+                image {
+                    childImageSharp {
+                        fluid(maxHeight: 500, quality: 90) {
+                            ...GatsbyImageSharpFluid_withWebp
+                        }
+                    }
+                }
+            }
         }
-      }
     }
-  }
 `;
