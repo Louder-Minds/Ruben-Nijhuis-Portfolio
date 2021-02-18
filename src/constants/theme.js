841516b7f4fc2d971aa11411d51709e6a778ec02
Ruben Nijhuis <contact@rubennijhuis.com>
2021-02-18 13:57:56 +0100

    🔧:style updated constants and helpers

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,1 +1,136 @@
-export const accent = '#f90000';
+/**
+ * Magic
+ */
+export const magicNumber = '72px';
+
+/**
+ * Colors
+ */
+export const mainDark = `#1E1E1E`;
+export const offsetDark = `#181818`;
+export const underline = `#C4C4C4`;
+export const highlight = `#00EFAC`;
+export const backgroundCover = `#FAFAFA`;
+export const darkBackWhite = `#FFFFFF`;
+
+/**
+ * Typography
+ */
+export const mainTitle = `75px`;
+export const h1 = `56px`;
+export const h2 = `42px`;
+export const subtitle = `32px`;
+export const bodyLarge = `24px`;
+export const bodyRegular = `18px`;
+export const bodySmall = `13px`;
+
+export const lineHeight = `1.5`;
+
+/**
+ * Radii
+ */
+export const imgBorderRadius = `9px`;
+export const regularBorderRadius = `6px`;
+
+/**
+ * Transitions
+ */
+export const underlineTransitionTime = `250ms`;
+export const underlineTransitionCurve = `0.58, 0.01, 0.9, 0.66`;
+
+export const contentBlockFadeinTime = `250ms`;
+export const contentBlockFadeinCurve = `0.58, 0.01, 0.9, 0.66`;
+
+export const pageTransitionRevealTime = `250ms`;
+export const pageTransitionRevealCurve = `0.58, 0.01, 0.9, 0.66`;
+
+/**
+ * Testing data
+ */
+export const imgTestLink = `https://source.unsplash.com/800x800`;
+
+export const personalTestdata = [
+    {
+        where: 'ROC Amstelland',
+        what: 'Teacher of design',
+        when: '06/21 • 04/22',
+    },
+    {
+        where: 'Het Amsterdams Lyceum',
+        what: 'Student',
+        when: '04/15 • 04/21',
+    },
+    {
+        where: 'Louder Minds',
+        what: 'Founder',
+        when: '10/20 • Present',
+    },
+    {
+        where: 'Teacher',
+        what: 'somewhere',
+        when: '21/02 • 22/01',
+    },
+    {
+        where: 'Teacher',
+        what: 'somewhere',
+        when: '21/02 • 22/01',
+    },
+    {
+        where: 'Teacher',
+        what: 'somewhere',
+        when: '21/02 • 22/01',
+    },
+    {
+        where: 'Teacher',
+        what: 'somewhere',
+        when: '21/02 • 22/01',
+    },
+    {
+        where: 'Teacher',
+        what: 'somewhere',
+        when: '21/02 • 22/01',
+    },
+];
+
+export const contentCardTestData = [
+    {
+        title: 'Studio Kffein',
+        tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
+        img: 'https://source.unsplash.com/800x750',
+        subjects: ['Web development', 'Fun', 'Cool people'],
+        type: 'projects',
+        full: true,
+    },
+    {
+        title: 'Ptppnz',
+        tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
+        img: 'https://source.unsplash.com/800x850',
+        subjects: ['Practice', 'Living lab', 'Experiments'],
+        type: 'projects',
+        full: false,
+    },
+    {
+        title: 'Kids Academy',
+        tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
+        img: 'https://source.unsplash.com/800x900',
+        subjects: ['New', 'Covid', 'Happy people'],
+        type: 'projects',
+        full: false,
+    },
+    {
+        title: 'Limbic.ai',
+        tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
+        img: 'https://source.unsplash.com/800x820',
+        subjects: ['Design', 'Developement', 'Cool stuff'],
+        type: 'projects',
+        full: false,
+    },
+    {
+        title: 'Studio Kffein',
+        tagline: 'Pinnacle overcome decieve marvelous decieve eternal-return.',
+        img: 'https://source.unsplash.com/800x810',
+        subjects: ['Lorem ipsum', 'Lorem ipsum', 'Lorem ipsum'],
+        type: 'projects',
+        full: false,
+    },
+];
